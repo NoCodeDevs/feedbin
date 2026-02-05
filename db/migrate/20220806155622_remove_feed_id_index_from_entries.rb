93a243237c3bc609c6488050ec87a2deb4ea0eaa
NoCodeDevs <119425316+NoCodeDevs@users.noreply.github.com>
2026-02-05 15:21:17 -0500

    hhh

--- db/migrate/20220806155622_remove_feed_id_index_from_entries.rb
+++ db/migrate/20220806155622_remove_feed_id_index_from_entries.rb
@@ -2,10 +2,10 @@ class RemoveFeedIdIndexFromEntries < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def up
-    remove_index :entries, :feed_id, algorithm: :concurrently
+    remove_index :entries, name: "index_entries_on_feed_id", algorithm: :concurrently if index_exists?(:entries, nil, name: "index_entries_on_feed_id")
   end
 
   def down
-    add_index :entries, :feed_id, algorithm: :concurrently
+    add_index :entries, :feed_id, algorithm: :concurrently, name: "index_entries_on_feed_id" unless index_exists?(:entries, :feed_id, name: "index_entries_on_feed_id")
   end
 end
